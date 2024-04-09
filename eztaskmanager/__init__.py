5cedf97060de8dfb351287a4a2804f622f045dbd
Guglielmo Celata <guglielmo@openpolis.it>
2024-04-09 16:40:33 +0200

    Bump version: 0.2.0 → 0.3.0

--- eztaskmanager/__init__.py
+++ eztaskmanager/__init__.py
@@ -1,7 +1,7 @@
 """Django application to manage async tasks via admin interface."""
 
 # PEP 440 - version number format
-VERSION = (0, 2, 0)
+VERSION = (0, 3, 0)
 
 # PEP 396 - module version variable
 __version__ = ".".join(map(str, VERSION))
